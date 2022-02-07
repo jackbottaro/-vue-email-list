3cd7837005f4f96c96caff6d4f43c03a0727a49f
jackbottaro <mail.giacomobottaro@gmail.com>
2022-02-07 15:32:01 +0100

    push

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
-console.log(vue);
+const root = new Vue({
+    el: "#root",
+    data: {
+        mails: [],
+        mailNum: 10,
+    },
+    methods: {
+        getRandomEmail(){ 
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(res => {
+                const mail = res.data.response;
+                this.emails.push(mail);
+            })
+        }
+    },
+    mounted() {
+        for (let i = 0; i > this.mailNum; i++) {
+            this.getRandomEmail()
+        }
+    }
+})
